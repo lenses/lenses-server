f60bd589abfb61d5d0a6222877a1c544bd4fbfcb
sepans <sepans@sepans.com>
2015-04-28 16:33:36 -0400

    lenses model

--- server/api/lens/lens.model.js
+++ server/api/lens/lens.model.js
@@ -3,10 +3,22 @@
 var mongoose = require('mongoose'),
     Schema = mongoose.Schema;
 
+// schema for input data pasted into lens
+var InputDataSchema = new Schema({
+	name: String,
+	keywords: [String],
+	data: Schema.Types.Mixed
+});
+
+module.exports = mongoose.model('InputData', InputDataSchema);
+
 var LensSchema = new Schema({
   name: String,
-  info: String,
-  active: Boolean
+  type: String,
+  structure: Schema.Types.Mixed,
+  active: Boolean,
+  inputData: {type: Schema.Types.ObjectId, ref: 'InputData'}
 });
 
 module.exports = mongoose.model('Lens', LensSchema);
+
