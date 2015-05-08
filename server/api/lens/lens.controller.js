72f152b345190444cca3392a69a6113c733351ec
Nisha Batra <nishabatra27@gmail.com>
2015-05-08 15:47:16 -0400

    fix update issue not working with objects and arrays

--- server/api/lens/lens.controller.js
+++ server/api/lens/lens.controller.js
@@ -31,15 +31,25 @@ exports.create = function(req, res) {
 // Updates an existing lens in the DB.
 exports.update = function(req, res) {
   if(req.body._id) { delete req.body._id; }
-  Lens.findById(req.params.id, function (err, lens) {
-    if (err) { return handleError(res, err); }
-    if(!lens) { return res.send(404); }
-    var updated = _.merge(lens, req.body);
-    updated.save(function (err) {
-      if (err) { return handleError(res, err); }
-      return res.json(200, lens);
-    });
+  if(req.body.__v || req.body.__v == 0) { delete req.body.__v; } // Version key should not be hardcoded and is updated interally. MongoDB will not allow it to be set manually, so remove it from data.
+
+  // findOneAndUpdate updates non-string attributes properly, whereas findById was not saving those values to the DB.
+  // Mongoose documentation: http://mongoosejs.com/docs/api.html#model_Model.findOneAndUpdate
+  Lens.findOneAndUpdate({_id: req.params.id}, req.body, {upsert: true}, function(err, lens){
+    if (err) { console.log(err); return handleError(res, err); }
+    return res.json(200, lens);
   });
+
+  // Removed because it was not updating non-string fields, like finalResult.
+  //   Lens.findById(req.params.id, function (err, lens) {
+  //     if (err) { return handleError(res, err); }
+  //     if(!lens) { return res.send(404); }
+  //     var updated = _.merge(lens, req.body);
+  //     updated.save(function (err) {  
+  //       if (err) { return handleError(res, err); }
+  //       return res.json(200, lens);
+  //     });
+  //   });
 };
 
 // Deletes a lens from the DB.
